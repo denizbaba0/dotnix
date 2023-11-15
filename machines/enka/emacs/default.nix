a23a9110510d145b206544fc2614238730a6fc09
utfeight <utfeightt@gmail.com>
2023-11-15 08:41:02 +0300

    feat: doomemacs

--- machines/enka/emacs/default.nix
+++ machines/enka/emacs/default.nix
@@ -1,34 +1,18 @@
 {
-  xdg,
-  programs,
+  systemPackages,
   pkgs,
-  enabled,
-  homeConfiguration,
   ...
-}:
-homeConfiguration "nixos" {
-  # For doom emacs to work, you'll need to clone the repo as per the instructions in the doomemacs/doomemacs repo
-  programs.emacs = enabled {
-    package = pkgs.emacs29-pgtk;
-    extraPackages = epkgs: [
-      epkgs.vterm
-    ];
-  };
-
-  # xdg.configFile.".doom.d" = {
-  #   target = "../.doom.d";
-  #   source = ./config;
-  #   recursive = true;
-  #   #onChange = "${pkgs.libnotify}/bin/notify-send -u critical 'DOOM EMACS' 'Changed detected when rebuilding system. Please make sure to run `doom sync`!'";
-  # };
-}
-# home.packages = with pkgs; [
-#   rust-analyzer #rust lsp
-#   nodePackages.bash-language-server #sh and bash
-#   zls #zig lsp
-#   rnix-lsp #nix lsp
-#   ccls # c and c++ lsp
-#   texlive.combined.scheme-medium #latex
-#   pandoc #document conversion
-#   graphviz #org roam graph generation
-# ];
+}: let
+  doom-emacs =
+    pkgs.callPackage (builtins.fetchTarball {
+      url = "https://github.com/nix-community/nix-doom-emacs/archive/master.tar.gz";
+    }) {
+      doomPrivateDir = ./config; # Directory containing your config.el, init.el
+      # and packages.el files
+    };
+in
+  # home.packages = [doom-emacs];
+  (with pkgs;
+    systemPackages [
+      doom-emacs
+    ])
