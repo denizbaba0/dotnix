8ac035cd14cb6361e2b70967f047760c2fd6c46f
utfeight <utfeightt@gmail.com>
2023-11-24 16:58:55 +0300

    feat: $EDITOR=nvim

--- machines/enka/nushell/default.nix
+++ machines/enka/nushell/default.nix
@@ -1,16 +1,23 @@
-{ lib, pkgs, systemConfiguration, homeConfiguration, homePackages, enabled, ... }: lib.recursiveUpdate3
-
+{
+  lib,
+  pkgs,
+  systemConfiguration,
+  homeConfiguration,
+  homePackages,
+  enabled,
+  ...
+}:
+lib.recursiveUpdate3
 (systemConfiguration {
   users.users.nixos.shell = pkgs.nushell;
-    environment.sessionVariables = rec {
+  environment.sessionVariables = rec {
     # Not officially in the specification
-    XDG_BIN_HOME    = "$HOME/.local/bin";
-    PATH = [ 
+    XDG_BIN_HOME = "$HOME/.local/bin";
+    PATH = [
       "${XDG_BIN_HOME}"
     ];
   };
 })
-
 (homeConfiguration "nixos" {
   programs.starship = enabled {
     enableNushellIntegration = true;
@@ -18,17 +25,22 @@
 
   programs.nushell = enabled {
     configFile.source = ./configuration.nu;
-    envFile.source    = ./environment.nu;
+    envFile.source = ./environment.nu;
 
     shellAliases = {
-      la  = "ls --all";
-      ll  = "ls --long";
+      la = "ls --all";
+      ll = "ls --long";
       lla = "ls --long --all";
-      sl  = "ls";
+      sl = "ls";
+    };
+
+    environmentVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
     };
   };
 })
-
-(with pkgs; homePackages "nixos" [
-  carapace
-])
+(with pkgs;
+  homePackages "nixos" [
+    carapace
+  ])
